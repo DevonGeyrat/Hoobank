536ca6fb2dfecd4b1dec390d9feee3744d0aec23
DevonGeyrat <45537599+DevonGeyrat@users.noreply.github.com>
2022-10-05 13:57:59 +0200

    upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = ({ styles }) => (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none rounded-lg ${styles}`}>
+        Get Started
+    </button>
+)
+
+
+export default Button
